9a235d28443d2ca97ecb8d6ab76a90e989dcdc7b
Sreeraj S Kurup <154326486+s-r-e-e-r-a-j@users.noreply.github.com>
2026-02-19 13:40:09 +0530

    Update and rename src/memdup_into.asm to fm_memstarts_with.asm

---
+++ fm_memstarts_with.asm
@@ -0,0 +1,29 @@
+; Developer: Sreeraj
+; GitHub: https://github.com/s-r-e-e-r-a-j
+
+global fm_memstarts_with
+section .text
+fm_memstarts_with:
+    cmp rcx, rsi    
+    ja .false
+
+.loop:
+    test rcx, rcx
+    jz .true
+    mov al, [rdi]
+    cmp al, [rdx]
+    jne .false
+    inc rdi
+    inc rdx
+    dec rcx
+    jmp .loop
+
+.true:
+    mov eax, 1
+    ret
+
+.false:
+    xor eax, eax
+    ret
+
+section .note.GNU-stack noalloc noexec nowrite
